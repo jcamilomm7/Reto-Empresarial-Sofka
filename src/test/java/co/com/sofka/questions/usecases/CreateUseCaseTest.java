2cba38bc784d2a1d8ee2585107ee876dda53450d
jcamilomm7 <88809617+jcamilomm7@users.noreply.github.com>
2022-06-11 17:47:04 -0500

    ADD --- Se agrega la dependencia h2 para poder realizar el test de createUseCase , simulando una base de datos y esta sale exitosa

--- src/test/java/co/com/sofka/questions/usecases/CreateUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/CreateUseCaseTest.java
@@ -1,7 +1,63 @@
 package co.com.sofka.questions.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+import reactor.test.StepVerifier;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class CreateUseCaseTest {
+    @Autowired
+    QuestionRepository questionRepository;
+
+    CreateUseCase createUseCase;
+
+
+    @BeforeEach
+    public void setup() {
+        MapperUtils mapperUtils = new MapperUtils();
+        //questionRepository = mock(QuestionRepository.class);
+
+        createUseCase = new CreateUseCase(mapperUtils, questionRepository);
+    }
+
+    @Test
+    void apply() {
+        var question = new Question();
+        question.setId("bbbb");
+        question.setUserId("xxxx-xxxx");
+        question.setType("tech");
+        question.setCategory("software");
+        question.setQuestion("¿Que es java?");
+        //when(questionRepository.save(question).thenReturn(Mono.just(question));
+
+        QuestionDTO questionDTO = new QuestionDTO(
+                question.getId(),
+                question.getUserId(), question.getQuestion(), question.getType(), question.getCategory()
+        );
+
+
+        StepVerifier.create(createUseCase.apply(questionDTO))
+                .expectNextMatches(questiondto-> {
+                    assert questionDTO.getUserId().equals("xxxx-xxxx");
+                    return true;
+                })
+                .verifyComplete();
+
+
+
+    }
+
 
 }
